35cda06d06f54b9f994119e658f4dafd05bd5d78
RetroMetalGamer <matiaserrigar87@gmail.com>
2026-04-30 10:30:54 +0100

    actu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,80 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    public float speed = 3.7f;
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float velocidadCarrera = 8f;
+    [SerializeField] private Vector2 direccion;
     private Rigidbody2D rb2d;
-    private Vector2 movementInput;
+    private float movimientoX;
+    private float movimientoY;
     private Animator animator;
-    public float side;
-    public Vector2 _input;
+    private bool corriendo;
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        movementInput.x = Input.GetAxisRaw("Horizontal");
-        movementInput.y = Input.GetAxisRaw("Vertical");
+        movimientoX = Input.GetAxisRaw("Horizontal");
+        movimientoY = Input.GetAxisRaw("Vertical");
 
-        movementInput = movementInput.normalized;
+        corriendo = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
 
-        animator.SetFloat("Horizontal", movementInput.x);
-        animator.SetFloat("Vertical", movementInput.y);
-        animator.SetFloat("Speed", movementInput.magnitude);
+        animator.SetFloat("MovimientoX", movimientoX);
+        animator.SetFloat("MovimientoY", movimientoY);
+        animator.SetBool("Corriendo", corriendo);
 
+        direccion = new Vector2(movimientoX, movimientoY).normalized;
 
-    }
-    public void Move()
-    {
-        side = _input.x;
-        float UpDown = _input.y;
-        if (side > 0)
+        if (movimientoX != 0 || movimientoY != 0)
         {
-            animator.SetBool("movimientoDerecha", true);
-            animator.SetBool("movimientoIzquierda", false);
+            animator.SetFloat("UltimoX", movimientoX);
+            animator.SetFloat("UltimoY", movimientoY);
         }
-        if (side == 0)
-        {
-            animator.SetBool("movimientoIzquierda", false);
-            animator.SetBool("movimientoDerecha", false);
-        }
-        if (side < 0)
-        {
-            animator.SetBool("movimientoDerecha", false);
-            animator.SetBool("movimientoIzquierda", true);
-        }
-        if (UpDown > 0)
-        {
-            animator.SetBool("arriba", true);
-            animator.SetBool("abajo", false);
-        }
-        if (UpDown == 0)
-        {
-            animator.SetBool("arriba", false);
-            animator.SetBool("abajo", false);
-        }
-        if (UpDown < 0)
-        {
-            animator.SetBool("arriba", false);
-            animator.SetBool("abajo", true);
-        }
-        if (side == 0 && UpDown == 0)//si lado y arriba abajo es 0, movimiento falso
-        {
-            animator.SetBool("moving", false);
-        }
-        if (side != 0 || UpDown != 0)//si lado o arriba abajo es diferente a 0, movimiento verdadero
-        {
-            animator.SetBool("moving", true);
-        }
-
     }
 
     private void FixedUpdate()
     {
-        rb2d.velocity = movementInput * speed;
+        float velocidadActual = corriendo ? velocidadCarrera : velocidadMovimiento;
+        rb2d.MovePosition(rb2d.position + direccion * velocidadActual * Time.fixedDeltaTime);
     }
 }
