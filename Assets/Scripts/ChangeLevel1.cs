73afdf9d3f333d6930d2cc9973065ba478aef29f
RetroMetalGamer <matiaserrigar87@gmail.com>
2026-04-30 13:26:27 +0100

    actu

--- Assets/Scripts/ChangeLevel1.cs
+++ Assets/Scripts/ChangeLevel1.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; // Biblioteca de cambio de escena
+using UnityEngine.SceneManagement;
 
 public class ChangeLevel1 : MonoBehaviour
 {
-    public void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("mapa", LoadSceneMode.Single);
-            //compara la etiqueta del player y carga de manera individual la siguiente escena
+            SceneManager.LoadScene("Mapa", LoadSceneMode.Single);
         }
     }
 }
