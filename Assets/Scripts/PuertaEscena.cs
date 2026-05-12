afb37a3eb2e48bd661086d5a128b202f64e35916
MaErGa (Matias) <matiaserrigar87@gmail.com>
2026-05-12 19:34:41 +0100

    actualizacion del inicio

---
+++ Assets/Scripts/PuertaEscena.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PuertaEscena : MonoBehaviour
+{
+    [Header("Configuración de la Escena")]
+    public string nombreEscenaDestino = "Underworld"; // Nombre de tu escena
+
+    // Este método se activa cuando algo entra en el objeto (la puerta)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Comprobamos si lo que tocó la puerta es el jugador
+        if (collision.CompareTag("Player"))
+        {
+            // Opcional: Si queremos que al entrar por una puerta NO cuente como retorno de combate
+            MovimientoMapa.vieneDeCombate = false; 
+
+            SceneManager.LoadScene(nombreEscenaDestino);
+        }
+    }
+}
